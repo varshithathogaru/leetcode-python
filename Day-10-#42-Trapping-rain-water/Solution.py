1ddc8109d83ef58541260d5a98b219ab1da4de87
varshithathogaru <varshithathogaru@gmail.com>
2026-08-13 21:22:59 +0530

    Implement water trapping algorithm in Solution.py

---
+++ Day-10-#42-Trapping-rain-water/Solution.py
@@ -0,0 +1,24 @@
+from typing import List
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        l = 0
+        r = len(height) - 1
+
+        leftmax = height[l]
+        rightmax = height[r]
+
+        res = 0
+
+        while l < r:
+            if leftmax < rightmax:
+                l += 1
+                leftmax = max(leftmax, height[l])
+                res += leftmax - height[l]
+
+            else:
+                r -= 1
+                rightmax = max(rightmax, height[r])
+                res += rightmax - height[r]
+
+        return res
