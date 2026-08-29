a92ea062a075359fc54e6dfdf1dddb4885a49fee
varshithathogaru <varshithathogaru@gmail.com>
2026-08-29 20:41:20 +0530

    Implement word pattern matching solution

---
+++ Day-24-#290- word-pattern/Solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+
+        if len(pattern) != len(words):
+            return False
+
+        char_to_word = {}
+        word_to_char = {}
+
+        for c, w in zip(pattern, words):
+
+            if c in char_to_word and char_to_word[c] != w:
+                return False
+
+            if w in word_to_char and word_to_char[w] != c:
+                return False
+
+            char_to_word[c] = w
+            word_to_char[w] = c
+
+        return True
